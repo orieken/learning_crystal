631ca8b28d439730f2ba887ed7539867eadd14f4
Oscar Rieken <oscar_rieken@homedepot.com>
2016-04-17 23:31:03 -0400

    got the number splitter working

--- spec/number_splitter_spec.cr
+++ spec/number_splitter_spec.cr
@@ -1,6 +1,22 @@
 require "./spec_helper"
 
 describe LearningCrystal::NumberSplitter do
-
-
+  context "split_hours" do
+    it "split 10 by 5" do    
+      total = LearningCrystal::NumberSplitter.split_numbers(10, 5)
+      total.should eq [5, 5]
+    end  
+    it "split 15 by 3" do    
+      total = LearningCrystal::NumberSplitter.split_numbers(15, 3)
+      total.should eq [3, 3, 3, 3, 3]
+    end
+    it "split 16 by 3" do    
+      total = LearningCrystal::NumberSplitter.split_numbers(16, 3)
+      total.should eq [3, 3, 3, 3, 3, 1]
+    end
+    it "split 17 by 3" do    
+      total = LearningCrystal::NumberSplitter.split_numbers(17, 3)
+      total.should eq [3, 3, 3, 3, 3, 2]
+    end
+  end
 end
--- src/learning_crystal/number_splitter.cr
+++ src/learning_crystal/number_splitter.cr
@@ -1,5 +1,16 @@
 module LearningCrystal
   class NumberSplitter 
-
+    def self.split_numbers(number, interval)
+      blocks = Array(Int32).new
+      repeat = number / interval
+      if number % interval == 0
+        repeat.times { blocks << interval}
+      elsif number % interval != 0
+        remainder = number - ((number / interval) * interval)
+        repeat.times { blocks << interval}
+        blocks << remainder        
+      end
+      blocks
+    end    
   end
 end
